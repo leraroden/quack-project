50a8ef3d5acfa87d1b820325c1487db6f647fe7d
Alla Spitzer <alla.spitzerr@gmail.com>
2023-06-13 22:30:59 +0200

    change UserController -> RegisterUserREST

--- quack-project-business/src/main/java/de/ls5/wt2/rest/RegisterUserREST.java
+++ quack-project-business/src/main/java/de/ls5/wt2/rest/RegisterUserREST.java
@@ -1,33 +1,36 @@
-package de.ls5.wt2.controller;
+package de.ls5.wt2.rest;
 
 import de.ls5.wt2.entity.User;
-import de.ls5.wt2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
-@RestController()
-public class UserController {
-
-    @Autowired
-    private UserRepository userRepository;
+@Transactional
+@RestController
+@RequestMapping(path = "rest/users")
+public class RegisterUserREST {
 
     @Autowired
     private EntityManager entityManager;
 
-    @PostMapping(path = "rest/users")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> addUser(@RequestBody User user) {
         if(user.getUsername() != null && user.getPassword() != null) {
             final TypedQuery<User> foundUsers= entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
                     .setParameter("username", user.getUsername());
             List<User> users = foundUsers.getResultList();
             if(users.isEmpty()){
-                userRepository.save(user);
+                entityManager.persist(user);
                 return new ResponseEntity<>(HttpStatus.OK);
             }
             return new ResponseEntity<>(HttpStatus.CONFLICT);
